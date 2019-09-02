82a68f708820caeb7cebed7f3e36c801b121464b
wfy1998 <1291800865@qq.com>
2019-09-02 01:29:40 -0400

    change main page

--- src/app/common/main/main.component.ts
+++ src/app/common/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
-  constructor() { }
+  loginUserData = {};
+  constructor(private _auth: AuthService, private _router: Router) { }
 
   ngOnInit() {
+  }
+
+  loginUser() {
+    this._auth.loginUser(this.loginUserData)
+      .subscribe(
+        res => {
+          localStorage.setItem('token', res.token);
+          this._router.navigate(['/']);
+        },
+        err => console.log(err)
+      );
   }
 
 }
