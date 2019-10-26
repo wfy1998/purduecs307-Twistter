6541226fe431a221548a0e2f650d3c0aadaab320
wfy1998 <1291800865@qq.com>
2019-10-26 17:22:46 -0400

    findUser

--- backend/routes/find.js
+++ backend/routes/find.js
@@ -5,6 +5,11 @@ const userModel = require('../models/User');
 const auth = require('../middleware/check-auth');
 
 router.post('/findUser', auth, (req, res) => {
+  userModel.findOne({username: req.body}, (err, user) => {
+    if(err) {
+      res.status(500)
+    }
+  })
 
 });
 
