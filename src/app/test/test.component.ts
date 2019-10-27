447bfce4c4c074a679dd6cd072ef85f4ba919fc0
wfy1998 <1291800865@qq.com>
2019-10-27 14:15:13 -0400

    test component

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import {AuthService} from '../auth.service';
+import {OtherService} from '../other.service';
 
 @Component({
   selector: 'app-test',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  constructor() { }
+  newPost = {
+    content: '',
+    userName: '',
+    tag: [],
+  };
+
+  newTag = {
+    tag: ''
+  };
+
+  constructor(private _auth: AuthService, private _router: Router, private _other: OtherService) { }
 
   ngOnInit() {
+  }
+
+  submitPost() {
+    this._other.createNewPost(this.newPost).subscribe(res => {
+      console.log('create post');
+    },
+    err => console.log(err)
+    );
+  }
+
+  submitNewTag() {
+    this._other.addNewTag(localStorage.getItem('username'), this.newTag).subscribe(res => {
+      console.log('add tag');
+    },
+      err => console.log(err)
+      );
   }
 
 }
