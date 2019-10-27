6888bc2c9b1efdce3f8547728795a01b89f310bc
Direct-eks <Direct-eks@users.noreply.github.com>
2019-10-27 13:29:04 -0400

    delete

--- backend/routes/delete.js
+++ backend/routes/delete.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const userModel = require('../models/User');
 
 const checkAuth = require('../middleware/check-auth');
 
 router.post('/', checkAuth, (req, res) => {
-
+  userModel.deleteOne({username: req.body.username}, (err) => {
+    if (err) {
+      res.status(500).send('no such user')
+    }
+  })
 });
 
 module.exports = router;
