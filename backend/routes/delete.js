e0d1895bd9d1f0d868ccfa44a02815100b07abc6
Direct-eks <35151167+Direct-eks@users.noreply.github.com>
2019-10-30 22:47:56 -0400

    deletion fix

--- backend/routes/delete.js
+++ backend/routes/delete.js
@@ -1,17 +1,60 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+
 const userModel = require('../models/User');
+const followModel = require('../models/Followed');
+const postModel = require('../models/Post');
 
 const checkAuth = require('../middleware/check-auth');
 
 router.post('/', checkAuth, (req, res) => {
-  userModel.deleteOne({username: res.locals.username}, (err) => {
-    if (err) {
-      res.status(500).send('no such user')
+
+  userModel.findOne({username: res.locals.username}, (err, user) => {
+    if (err) {console.log(err); res.status(500).send(); return}
+    if (!user) {res.status(403).send(); return}
+
+    let followData = user.userFollowed;
+    let postsData = user.userPosts;
+
+    for (let temp of followData) {
+      followModel.findByIdAndDelete(temp, (err) => {
+        if (err) {
+          console.log(err);
+          console.log('deletion failed for' + temp)
+        }
+      });
     }
-    res.status(200);
-  })
+
+    for (let temp of postsData) {
+      postModel.findByIdAndDelete(temp, (err) => {
+        if (err) {
+          console.log(err);
+          console.log('deletion failed for' + temp)
+        }
+      })
+    }
+
+    console.log('sleeping for 5 seconds!');
+    //sleeping and wait for callbacks to finish
+    const sleep = (milliseconds) => {
+      return new Promise(resolve => setTimeout(resolve, milliseconds))
+    };
+    sleep(5000).then(() => {
+      console.log('now hope all callbacks have finished!');
+      userModel.deleteOne({username: res.locals.username}, (err) => {
+        if (err) {
+          console.log('deletion error!');
+          res.status(500).send();
+          return
+        }
+        res.status(200);
+      })
+    });
+
+  });
+
+
 });
 
 module.exports = router;
