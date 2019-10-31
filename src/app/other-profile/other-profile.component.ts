60141b297b5067a5c30a2a90af04a6f652ad0188
Direct-eks <Direct-eks@users.noreply.github.com>
2019-10-31 15:57:12 -0400

    follow error handle

--- src/app/other-profile/other-profile.component.ts
+++ src/app/other-profile/other-profile.component.ts
@@ -63,6 +63,17 @@ export class OtherProfileComponent implements OnInit {
     this.follow.username = this._activateroute.snapshot.params.username;
     this._other.followUser(this.follow).subscribe(res => {
       console.log('follow success');
+    }, err => {
+      if (err.status === 400) {
+        alert('Bad request! Please fill in all the blanks');
+      } else if (err.status === 403) {
+        alert('User not found');
+      } else if (err.status === 406) {
+        alert('Repeated Follow!');
+      } else if (err.status === 500) {
+        alert('Server Error!');
+      }
+      console.log(err);
     });
 
   }
