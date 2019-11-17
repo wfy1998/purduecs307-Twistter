c2044bc4093d8a231d3df3496510190d79c09ed3
Direct-eks <Direct-eks@users.noreply.github.com>
2019-11-17 13:04:41 -0500

    Delete Conversation.js

--- backend/models/Conversation.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const Conversation = mongoose.Schema({
-  firstUsername: {type: String},
-  secondUsername: {type: String},
-  firstUserMessages: {type: [String]},
-  secondUserMessages: {type: [String]}
-});
-module.exports = mongoose.model('Conversation', Conversation, 'Conversation');
